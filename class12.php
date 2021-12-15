4c5f32dead06b3ca394044808a40fc522ae06d17
Arjun Rokka <87361716+CodewothArjun@users.noreply.github.com>
2021-12-15 23:54:31 +0545

    added

--- class12.php
+++ class12.php
@@ -84,3 +84,71 @@ print_r($fruits);
 </body>
 </html>
 
+
+
+
+
+
+<?php
+
+// PHP program to find the largest and smallest element in an array by using for loop
+
+$x = [73, 42, 423, 42, 342, 14];
+
+// $x - denotes array list of input numbers
+// $b - denotes big element variable
+// $sm - denotes small element variable
+
+// This will stored the first element of the array to check the element
+$b = $x[0];
+
+for ($i = 1; $i < count($x); $i++) {
+    
+    // This will check one by one with each value of array
+    // To compare the value is smallest or largest
+    if ($b < $x[$i]) {
+        $b = $x[$i];
+    }
+}
+
+echo "\n-----The largest element is: ", $b, "\n";
+
+// This will stored the first element of the array to check the element
+$sm = $x[0];
+
+for ($i = 1; $i < count($x); $i++) {
+    
+    // This will check one by one with each value of array
+    // To compare the value is smallest or largest
+    if ($sm > $x[$i]) {
+        $sm = $x[$i];
+    }
+}
+
+echo "\n-----The smallest element is: ", $sm, "\n";
+
+?>
+
+
+<?php
+function primenumber($MyNum) {
+  $n = 0;
+
+  for($i = 2; $i < ($MyNum/2+1); $i++) {
+    if($MyNum % $i == 0){
+      $n++;
+      break;
+    }
+  }
+
+  if ($n == 0){
+    echo $MyNum." ";
+  } 
+}
+
+$x =100;
+echo "Prime numbers less than ".$x." are: \n";
+for($i = 2; $i < $x + 1; $i++) {
+  primenumber($i);
+}
+?>  -->
